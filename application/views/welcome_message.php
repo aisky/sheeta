670e5578f50f7485dec129220e50e1e3fd8c1761
skylau <skylau@SkymatoMacBook-Air.local>
2014-08-14 01:32:28 +0800

    menu
    add menu.php

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -13,56 +13,8 @@
 </head>
 <body>
 
-<section id="asideMenu" class="aside-menu">
-	<form class="form-inline form-search">
-        <div class="input-group">
-            <input type="search" class="form-control" placeholder="Search..." />
-            <span class="input-group-btn">
-                <button id="btnHideAsideMenu" class="btn btn-close" type="button" title="Hide sidebar"><i class="fa fa-times"></i></button>
-            </span>
-        </div>
-    </form>
-	
-    <h5 class="side-section-title">Optional sidebar menu</h5>
-    <div class="nav">
-    	<ul>
-        	<li>
-            	<a href="#">Home</a>
-            </li>
-            <li>
-            	<a href="#">About us</a>
-            </li>
-            <li>
-            	<a href="#">Blog</a>
-            </li>
-            <li>
-            	<a href="#">Work</a>
-            </li>
-            <li>
-            	<a href="#">Online shop</a>
-            </li>
-        </ul>
-    </div>
-    
-    <h5 class="side-section-title">Social media</h5>
-    <div class="social-media">
-        <a href="#"><i class="fa fa-facebook facebook"></i></a>
-        <a href="#"><i class="fa fa-google-plus google"></i></a>
-        <a href="#"><i class="fa fa-twitter twitter"></i></a>
-    </div>
-    
-    <h5 class="side-section-title">Contact information</h5>
-    <div class="contact-info">
-        <h5>Address</h5>
-        <p>5th Avenue, New York - United States</p>
-        
-        <h5>Email</h5>
-        <p>hello@webpixels.ro</p>
-        
-        <h5>Phone</h5>
-        <p>+10 724 1234 567</p>
-    </div>
-</section>
+<?php include_once 'menu.php';?>
+
 <div class="wrapper">
 	<!-- This section is only for demonstration purpose only. Just remove the div "divStyleSwitcher" -->
 <div id="divStyleSwitcher" class="style-switcher-slidebar">
