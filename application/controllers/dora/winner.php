03639ac2bef3557b61e1b8b5105e347a97aae77f
sk8305 <sk8305@gmail.com>
2014-12-14 05:46:40 +0800

    winner
    获取代码基本完成

--- application/controllers/dora/winner.php
+++ application/controllers/dora/winner.php
@@ -12,7 +12,7 @@ class Winner extends CI_Controller {
 		parent::__construct();
 	}
 
-	public function index()
+	public function index( $id='' )
 	{
 		//抓取这个页面
 		//http://www.365winner.biz/allArticle.aspx
@@ -26,18 +26,27 @@ class Winner extends CI_Controller {
 
 		}*/
 
-		foreach( $this->_list($url) as $v )
-		{
-			$this->_content( $this->site . 'Article.aspx?id=' . $v );
+        if( $id > 0 )
+        {
+            $arr = $this->_content( $this->site . 'Article.aspx?id=' . $id );
+            $this->_save($arr);
+        }
+        else
+        {
+            foreach( $this->_list($url) as $v )
+            {
+                $arr = $this->_content( $this->site . 'Article.aspx?id=' . $v );
 
-			echo '<hr>';
-			die;
-		}
+                $this->_save($arr);
 
+                echo '<hr>';
+                die;
+            }
+        }
 	}
 
 	//command preg_match
-	function _preg_match($preg, $content, $is_arr=true)
+	function _preg_match($preg, $content, $is_arr=false)
 	{
 		preg_match_all($preg, $content, $match);
 		if($is_arr)
@@ -54,22 +63,42 @@ class Winner extends CI_Controller {
 	function _list($url)
 	{
 		$content = file_get_contents($url);
+        //限制在列表div内
+        $content = $this->_preg_match('/<div class="gambling_tips_list">(.*?)<\/div>/is', $content);
 		//保留article 识别度高些
-		return $this->_preg_match('/<a href="Article\.aspx\?id=(.+)" title=".+" target="_blank">/', $content);
+		return $this->_preg_match('/<a href="Article\.aspx\?id=(.+)" title=".+" target="_blank">/', $content, true);
 	}
 
 	//get content
 	function _content($url)
 	{
-		$content = file_get_contents($url);
+        $return = array();
+        $return['url'] = $url;
 
+        $content = file_get_contents($url);
 		//day
-		$day = $this->_preg_match('%<em>发布时间：</em><span>(.+)</span>%', $content, false);
+		$return['day'] = $this->_preg_match('%<em>发布时间：</em><span>(.+)</span>%', $content);
 
 		//news_content
-//		echo $new_content = $this->_preg_match('%<div id="post_con">(*)</div>%', $content, false);
-		preg_match_all('%<div id="post_con">*</div>%', $content, $match);
+		$return['content'] = $this->_filter( $this->_preg_match('/<div id="post_con">(.*?)(<!-- Baidu Button BEGIN -->|<p>相关阅读)/is', $content) );
 
-		print_r($match);
+        //tagname
+        $tag_div = $this->_preg_match('/<div class="crumb">(.*?)<\/div>/is', $content);
+        $tag_a = $this->_preg_match('/<a.+?>(.*?)<\/a>/is', $tag_div, true);
+        $return['tagname'] = isset($tag_a[1]) ? $tag_a[1] : '';
+
+        return $return;
 	}
+
+    //过滤
+    function _filter($content)
+    {
+        return preg_replace('%<p style="text-align:center;"><img src=".+? /></p>%s', '', $content);
+    }
+
+    //保存
+    function _save($news_arr)
+    {
+        print_r($news_arr);
+    }
 }
